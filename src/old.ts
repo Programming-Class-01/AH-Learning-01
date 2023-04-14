0efc7ed952a285f59ca7707af2c9b156895d9e39
Amber Horky <corollax@gmail.com>
2023-04-14 23:26:22 +0000

    Synchronize repository to end state from last class, in preparation for review on functions and loops.

---
+++ src/old.ts
@@ -0,0 +1,9 @@
+// Variable: A discrete location in memory reserved for the storage of data for a specific period of time
+let firstName;
+{
+    // Object: A variable containing a tree of list of key/value pairs. 
+    const ENORMOUS_FILE = {name: "Amber", age: "34"};
+    firstName = ENORMOUS_FILE.name;
+}
+
+console.log(`Hello World's Cutest ${firstName}`);
